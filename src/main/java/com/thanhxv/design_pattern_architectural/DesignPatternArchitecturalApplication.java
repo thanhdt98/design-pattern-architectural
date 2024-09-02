478758b6b5791ca964793ecf1570c4f2c5f04f9d
thanhdt98 <thanhxv14.doaz@gmail.com>
2024-09-02 10:43:00 +0700

    init

---
+++ src/main/java/com/thanhxv/design_pattern_architectural/DesignPatternArchitecturalApplication.java
@@ -0,0 +1,13 @@
+package com.thanhxv.design_pattern_architectural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternArchitecturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternArchitecturalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanhxv/design_pattern_architectural/DesignPatternArchitecturalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhxv.design_pattern_architectural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternArchitecturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
